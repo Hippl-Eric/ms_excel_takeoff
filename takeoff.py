5ff4ff61be9606ecdf24a02ca0d0a3d266ab9a36
Eric Hippler <hippler.eric@gmail.com>
2020-07-13 20:19:22 -0400

    working cell manipulation, TODO: copy cell contents and style

--- takeoff.py
+++ takeoff.py
@@ -4,29 +4,35 @@ def main():
 
     # Request and parse arguments
     project_name = "test project name 123"
-    num_rows = 25
+    num_rows = 10
     drilled = False
 
     # Load base template file
     wb = load_workbook(filename = 'BASE Takeoff.xlsx')
     ws = wb["Takeoff-SB"]
 
-    name = ws['A1']
-
+    # Input project name
     ws['A1'] = project_name
 
-    file_name = f"Takeoff - {project_name}.xlsx"
-
-    wb.save(file_name)
-
-    # Input project name
-
     # Input drilled or driven
+    if drilled:
+        ws['F2'] = "DRILLED"
+    else:
+        ws["F2"] = "DRIVEN"
 
     # Insert rows
+    ws.insert_rows(6, num_rows)
+
+
+    # TODO
+    # Set the new print area
+    # ws.print_area = 'A1:F10'
 
     # Save in new project directory
+    file_name = f"Takeoff - {project_name}.xlsx"
+    wb.save(file_name)
 
+    # Success
     return print("Success")
 
 if __name__ == "__main__":
