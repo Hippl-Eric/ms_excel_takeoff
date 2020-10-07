bcc027ed02c6f74b7b11188e2faad1f52277246d
Eric Hippler <hippler.eric@gmail.com>
2020-10-07 10:31:44 -0400

    Workbook creation and varibles broken out into setUp

--- test_takeoff.py
+++ test_takeoff.py
@@ -8,43 +8,48 @@ import takeoff
 
 class TestHelperFunctions(unittest.TestCase):
     def setUp(self):
-        self.test_dir = os.getenv("TEST_DIR")
-        self.test_file = f"\\{os.getenv('TEST_FILE')}"
-        self.check_file = f"\\{os.getenv('CHECK_FILE')}"
-        self.num_rows = int(os.getenv("NUM_ROWS"))
-        self.project_name = "Unit_Test"
+
+        # Grab and set inputs
+        test_dir = os.getenv("TEST_DIR")
+        test_file = f"\\{os.getenv('TEST_FILE')}"
+        check_file = f"\\{os.getenv('CHECK_FILE')}"
+        num_rows = int(os.getenv("NUM_ROWS"))
+        project_name = "Unit_Test"
         if os.getenv("DRILLED") == "True":
-            self.drilled = True
+            drilled = True
         else:
-            self.drilled = False
+            drilled = False
+        
+        # Create a takeoff (excel) file to test
+        takeoff.create_new_takeoff(test_file, project_name, num_rows, drilled, test_dir, test_dir)
+
+        # Load the test workbook
+        self.test_wb = load_workbook(filename = f"{test_dir}//Takeoff - Unit_Test.xlsx")
+        self.test_ws = self.test_wb["Takeoff-SB"]
+        self.test_cells = tuple(self.test_ws)
+
+        # Load the check workbook
+        self.check_wb = load_workbook(filename = f"{test_dir}//{check_file}")
+        self.check_ws = self.check_wb["Takeoff-SB"]
+        self.check_cells = tuple(self.check_ws)
 
     def tearDown(self):
         pass
 
     def test_create_new_takeoff(self):
 
-        takeoff.create_new_takeoff(self.test_file, self.project_name, self.num_rows, self.drilled, self.test_dir, self.test_dir)
-
-        test_wb = load_workbook(filename = f"{self.test_dir}//Takeoff - Unit_Test.xlsx")
-        test_ws = test_wb["Takeoff-SB"]
-        test_cells = tuple(test_ws)
-
-        check_wb = load_workbook(filename = f"{self.test_dir}//{self.check_file}")
-        check_ws = check_wb["Takeoff-SB"]
-        check_cells = tuple(check_ws)
-
         # Check num rows
-        self.assertEqual(len(test_cells), len(check_cells))
+        self.assertEqual(len(self.test_cells), len(self.check_cells))
 
         # Check num columns
-        self.assertEqual(len(test_cells[0]), len(check_cells[0]))
+        self.assertEqual(len(self.test_cells[0]), len(self.check_cells[0]))
 
         # Check print area
-        self.assertEqual(test_ws.print_area[0], check_ws.print_area[0])
+        self.assertEqual(self.test_ws.print_area[0], self.check_ws.print_area[0])
 
         # Check each cell
         style_list = ["alignment", "border", "fill", "font", "number_format", "protection", "quotePrefix"]
-        for test_row, check_row in zip(test_cells, check_cells):
+        for test_row, check_row in zip(self.test_cells, self.check_cells):
             for test_cell, check_cell in zip(test_row, check_row):
 
                 # Check value, coordinate, and has_style
